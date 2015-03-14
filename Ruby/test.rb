221ea3ef6c5ca027b3e218aacc149928b480679d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-03-14 14:18:13 +0100

    Doing a simple console program

--- Ruby/test.rb
+++ Ruby/test.rb
@@ -1,3 +1,26 @@
-def add_them_all(x, y)
-	x + y
+keepGoing = true
+foundNumber = false
+numberToGuess = rand(10)
+response = ''
+while keepGoing
+	puts 'Find the magic number between O and 9'
+	proposedN = gets.chomp.to_i
+	if proposedN == numberToGuess
+		puts "You found the correct number : #{numberToGuess}"
+		puts 'Another try ? (Y/N)'
+		numberToGuess = rand(10)
+		response = gets.chomp
+		while response != 'N' and response != 'Y'
+			puts 'Please fill in either Y or N'
+			response = gets.chomp
+		end
+	else
+		puts 'Not correct. Please, try again'
+	end
+
+	if response == 'N'
+		keepGoing = false
+	else
+		keepGoing = true
+	end
 end
