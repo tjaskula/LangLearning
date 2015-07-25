96afcc7fd762d3d5d700296c687ed849ee8d0d8a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-07-25 22:54:29 +0200

    Refactor of the first sample

--- Ruby/CH1consoleNumbers.rb
+++ Ruby/CH1consoleNumbers.rb
@@ -1,11 +1,15 @@
 keepGoing = true
-foundNumber = false
 numberToGuess = rand(10)
+firstRun = true
 response = ''
 while keepGoing
-	puts 'Find the magic number between O and 9'
+	if firstRun
+		puts 'Find the magic number between O and 9'
+		firstRun = false
+	end
 	proposedN = gets.chomp.to_i
 	if proposedN == numberToGuess
+		firstRun = true
 		puts "You found the correct number : #{numberToGuess}"
 		puts 'Another try ? (Y/N)'
 		numberToGuess = rand(10)
@@ -14,9 +18,13 @@ while keepGoing
 			puts 'Please fill in either Y or N'
 			response = gets.chomp
 		end
+		response == 'N' ? keepGoing = false : keepGoing = true
+		response = ''
 	else
-		puts 'Not correct. Please, try again'
+		if proposedN > numberToGuess 
+			puts 'Too high'
+		else
+			puts 'Too low' 
+		end
 	end
-
-	response == 'N' ? keepGoing = false : keepGoing = true
 end
