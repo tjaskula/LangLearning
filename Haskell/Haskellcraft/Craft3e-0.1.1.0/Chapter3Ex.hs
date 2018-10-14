5e03546c789d90beea8bfc8ca4eec4e33367f853
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-10-14 22:42:35 +0200

    Excercie 3.1

---
+++ Haskell/Haskellcraft/Craft3e-0.1.1.0/Chapter3Ex.hs
@@ -0,0 +1,12 @@
+module Chapter3Ex where
+
+{-
+    3.1 Give another version of the definition of 'exclusive or' which works informally like this:
+    'exclusive or of x and y will be True if either x is True and y is False or x is False and y is True'.
+-}
+
+exOr :: Bool -> Bool -> Bool
+exOr x y
+    | x && not y = True
+    | not x && y = True
+    | otherwise = False
